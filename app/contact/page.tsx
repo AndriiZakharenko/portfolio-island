2454359e1c5b5a28e581c7763c56a1e7e73c3827
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-01-30 23:31:11 +0200

    feat-contact: form, api

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,118 @@
-const Contact = () => {
-    return (
-        <div>Contact</div>
-    )
-}
+"use client";
 
-export default Contact
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+
+const Contact = () => {
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "",
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "",
+        {
+          from_name: form.name,
+          to_name: "Andrii",
+          from_email: form.email,
+          to_email: "andriizakharenko@gmail.com",
+          message: form.message,
+        },
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || ""
+      )
+      .then(
+        () => {
+          setLoading(false);
+          //   showAlert({
+          //     show: true,
+          //     text: "Thank you for your message 😃",
+          //     type: "success",
+          //   });
+
+          setTimeout(() => {
+            // hideAlert(false);
+            setForm({
+              name: "",
+              email: "",
+              message: "",
+            });
+          }, 3000);
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+          //   showAlert({
+          //     show: true,
+          //     text: "I didn't receive your message 😢",
+          //     type: "danger",
+          //   });
+        }
+      );
+  };
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+          ref={formRef}
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John"
+              required
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="John@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              rows={4}
+              className="textarea"
+              placeholder="Write your thoughts here..."
+              value={form.message}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit" className="btn">
+            {loading ? "Sending..." : "Submit"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
