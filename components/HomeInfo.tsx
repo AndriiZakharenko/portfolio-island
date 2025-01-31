c388b199b2e3f8ff2c733bca296b46575d5a4f3d
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-01-31 21:29:12 +0200

    feat-popup: main message

---
+++ components/HomeInfo.tsx
@@ -0,0 +1,13 @@
+const HomeInfo = () => {
+
+    return (
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+        Hi, I&apos;m
+        <span className="font-semibold mx-2 text-white">Andrii</span>
+        👋
+        <br />a Software Engineer from Ukraine 🇺🇦
+      </h1>
+    );
+};
+
+export default HomeInfo;
