6c8fd24708a29dbc8128d795bff5ae3100b226a2
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-04 14:14:58 +0200

    feat-main-sky: sky rotation

--- app/page.tsx
+++ app/page.tsx
@@ -129,7 +129,7 @@ export default function Home() {
           />
 
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
 
           <Island
             position={islandPosition}
