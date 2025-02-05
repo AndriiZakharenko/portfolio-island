8f73c88324032d56d742e9421b58fc462860dc7f
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-05 23:48:49 +0200

    feat-footer: social links

---
+++ constants/socialLinks/index.ts
@@ -0,0 +1,19 @@
+import { contact, github, linkedin } from "@/public/icons";
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/AndriiZakharenko",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/andrii-zakharenko",
+  },
+];
--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,7 +1,31 @@
+import { socialLinks } from "@/constants/socialLinks";
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
     return (
-        <footer>Footer</footer>
-    )
+        <footer className='footer font-poppins'>
+          <hr className='border-slate-200' />
+    
+          <div className='footer-container'>
+            <p>
+              © 2025 <strong>Andrii Zakharenko</strong>. All rights reserved.
+            </p>
+    
+            <div className='flex gap-3 justify-center items-center'>
+              {socialLinks.map((link) => (
+                <Link key={link.name} href={link.link} target='_blank'>
+                  <Image
+                    src={link.iconUrl}
+                    alt={link.name}
+                    className='w-6 h-6 object-contain'
+                  />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </footer>
+      );
 }
 
 export default Footer
