6bf1aa85e4e6affca0c7608763b51965d7758e47
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-13 13:36:29 +0200

    hotfix-experience: change order

--- constants/experiences/index.ts
+++ constants/experiences/index.ts
@@ -2,29 +2,18 @@ import { freelance, hiwayarts, mate, terrafootball } from "@/public/images";
 
 export const experiences = [
   {
-    title: "Junior Frontend Developer",
-    company_name: "Terrafootball",
-    icon: terrafootball,
-    iconBg: "#2b6ab1",
-    date: "Mar 2019 - Mar 2022",
+    title: "Frontend Developer",
+    company_name: "Freelance",
+    icon: freelance,
+    iconBg: "#fbab18",
+    date: "Nov 2024 - Present",
     points: [
-      "Developing and implementing user interfaces using HTML, CSS, and JavaScript, ensuring responsive design across various devices;.",
-      "Customize Joomla templates, modules, and components using PHP, HTML, and CSS.",
-      "Identifying and fix UI bugs to enhance user experience.",
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
-  {
-    title: "Junior Frontend Developer",
-    company_name: "Hiwayarts",
-    icon: hiwayarts,
-    iconBg: "#b7e4c7",
-    date: "Apr 2023 - Dec 2023",
-    points: [
-      "Developing and customizing WordPress themes using HTML, CSS, JavaScript, and PHP.",
-      "Modifying existing themes to adjust layout, design elements, and features according to project requirements.",
-      "Integrating and customizing WordPress plugins to enhance functionality without affecting site performance.",
-    ],
-  },
+
   {
     title: "Intern Frontend Developer",
     company_name: "Mate Academy",
@@ -37,16 +26,30 @@ export const experiences = [
       "Practicing responsive design techniques and exploring ways to ensure cross-browser compatibility.",
     ],
   },
+
   {
     title: "Frontend Developer",
-    company_name: "Freelance",
-    icon: freelance,
-    iconBg: "#fbab18",
-    date: "Nov 2024 - Present",
+    company_name: "Hiwayarts",
+    icon: hiwayarts,
+    iconBg: "#b7e4c7",
+    date: "Apr 2023 - Dec 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developing and customizing WordPress themes using HTML, CSS, JavaScript, and PHP.",
+      "Modifying existing themes to adjust layout, design elements, and features according to project requirements.",
+      "Integrating and customizing WordPress plugins to enhance functionality without affecting site performance.",
+    ],
+  },
+
+  {
+    title: "Frontend Developer",
+    company_name: "Terrafootball",
+    icon: terrafootball,
+    iconBg: "#2b6ab1",
+    date: "Mar 2019 - Mar 2022",
+    points: [
+      "Developing and implementing user interfaces using HTML, CSS, and JavaScript, ensuring responsive design across various devices;.",
+      "Customize Joomla templates, modules, and components using PHP, HTML, and CSS.",
+      "Identifying and fix UI bugs to enhance user experience.",
     ],
   },
 ];
